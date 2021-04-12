756f809079d42f051b0ba27a5bfd839f72f11cde
Derek Au <derek@derekau.net>
2021-04-12 11:33:23 -0600

    types

--- machine-learning/heatmaps/typeheatmaps.php
+++ machine-learning/heatmaps/typeheatmaps.php
@@ -1,90 +1,92 @@
 <?php
 
+
 $glaze_types = [
-    460 => array( 'id' => 460, 'parent_id' => 100, 'name' => 'Glaze', 'concatenated_name' => 'Glaze'),
-    470 => array( 'id' => 470, 'parent_id' => 460, 'name' => 'Clear', 'concatenated_name' => 'Clear'),
-    480 => array( 'id' => 480, 'parent_id' => 460, 'name' => 'White, Off-White', 'concatenated_name' => 'White, Off-White'),
-    490 => array( 'id' => 490, 'parent_id' => 460, 'name' => 'Iron', 'concatenated_name' => 'Iron'),
-    500 => array( 'id' => 500, 'parent_id' => 490, 'name' => 'Celadon', 'concatenated_name' => 'Iron - Celadon'),
-    510 => array( 'id' => 510, 'parent_id' => 500, 'name' => 'Blue', 'concatenated_name' => 'Iron - Celadon - Blue'),
-    520 => array( 'id' => 520, 'parent_id' => 500, 'name' => 'Green', 'concatenated_name' => 'Iron - Celadon - Green'),
-    530 => array( 'id' => 530, 'parent_id' => 500, 'name' => 'Yellow', 'concatenated_name' => 'Iron - Celadon - Yellow'),
-    533 => array( 'id' => 533, 'parent_id' => 500, 'name' => 'Chun, Jun', 'concatenated_name' => 'Iron - Celadon - Chun'),
-    535 => array( 'id' => 535, 'parent_id' => 490, 'name' => 'Amber', 'concatenated_name' => 'Iron - Amber'),
-    540 => array( 'id' => 540, 'parent_id' => 490, 'name' => 'Tenmoku, Temmoku, Tianmu', 'concatenated_name' => 'Iron - Tenmoku'),
-    550 => array( 'id' => 550, 'parent_id' => 490, 'name' => 'Tea Dust', 'concatenated_name' => 'Iron - Tea Dust'),
-    560 => array( 'id' => 560, 'parent_id' => 490, 'name' => 'Tessha, Hare\'s Fur', 'concatenated_name' => 'Iron - Tessha, Hare\'s Fur'),
-    570 => array( 'id' => 570, 'parent_id' => 490, 'name' => 'Kaki, Tomato Red', 'concatenated_name' => 'Iron - Kaki, Tomato Red'),
-    580 => array( 'id' => 580, 'parent_id' => 490, 'name' => 'Oil Spot', 'concatenated_name' => 'Iron - Oil Spot'),
-    585 => array( 'id' => 585, 'parent_id' => 490, 'name' => 'Slip-Based', 'concatenated_name' => 'Iron - Slip-Based'),
-    590 => array( 'id' => 590, 'parent_id' => 460, 'name' => 'Shino', 'concatenated_name' => 'Shino'),
-    600 => array( 'id' => 600, 'parent_id' => 590, 'name' => 'Traditional', 'concatenated_name' => 'Shino - Traditional'),
-    610 => array( 'id' => 610, 'parent_id' => 590, 'name' => 'Carbon Trap', 'concatenated_name' => 'Shino - Carbon Trap'),
-    620 => array( 'id' => 620, 'parent_id' => 590, 'name' => 'High-Alumina', 'concatenated_name' => 'Shino - High-Alumina'),
-    630 => array( 'id' => 630, 'parent_id' => 590, 'name' => 'White', 'concatenated_name' => 'Shino - White'),
-    635 => array( 'id' => 635, 'parent_id' => 460, 'name' => 'Red', 'concatenated_name' => 'Red'),
-    640 => array( 'id' => 640, 'parent_id' => 635, 'name' => 'Copper', 'concatenated_name' => 'Red - Copper'),
-    650 => array( 'id' => 650, 'parent_id' => 640, 'name' => 'Oxblood', 'concatenated_name' => 'Red - Copper - Oxblood'),
-    660 => array( 'id' => 660, 'parent_id' => 640, 'name' => 'Flambe', 'concatenated_name' => 'Red - Copper - Flambe'),
-    670 => array( 'id' => 670, 'parent_id' => 640, 'name' => 'Peach Bloom', 'concatenated_name' => 'Red - Copper - Peach Bloom'),
-    673 => array( 'id' => 673, 'parent_id' => 635, 'name' => 'Pink', 'concatenated_name' => 'Red - Pink'),
-    675 => array( 'id' => 675, 'parent_id' => 635, 'name' => 'Stain', 'concatenated_name' => 'Red - Stain'),
-    680 => array( 'id' => 680, 'parent_id' => 460, 'name' => 'Green', 'concatenated_name' => 'Green'),
-    690 => array( 'id' => 690, 'parent_id' => 680, 'name' => 'Copper', 'concatenated_name' => 'Green - Copper'),
-    700 => array( 'id' => 700, 'parent_id' => 680, 'name' => 'Oribe', 'concatenated_name' => 'Green - Oribe'),
-    710 => array( 'id' => 710, 'parent_id' => 680, 'name' => 'Chrome', 'concatenated_name' => 'Green - Chrome'),
-    720 => array( 'id' => 720, 'parent_id' => 680, 'name' => 'Titanium', 'concatenated_name' => 'Green - Titanium'),
-    730 => array( 'id' => 730, 'parent_id' => 680, 'name' => 'Nickel', 'concatenated_name' => 'Green - Nickel'),
-    740 => array( 'id' => 740, 'parent_id' => 680, 'name' => 'Stain', 'concatenated_name' => 'Green - Stain'),
-    745 => array( 'id' => 745, 'parent_id' => 460, 'name' => 'Turquoise', 'concatenated_name' => 'Turquoise'),
-    750 => array( 'id' => 750, 'parent_id' => 460, 'name' => 'Blue', 'concatenated_name' => 'Blue'),
-    760 => array( 'id' => 760, 'parent_id' => 750, 'name' => 'Cobalt', 'concatenated_name' => 'Blue - Cobalt'),
-    770 => array( 'id' => 770, 'parent_id' => 750, 'name' => 'Rutile', 'concatenated_name' => 'Blue - Rutile'),
-    780 => array( 'id' => 780, 'parent_id' => 750, 'name' => 'Barium', 'concatenated_name' => 'Blue - Barium'),
-    790 => array( 'id' => 790, 'parent_id' => 750, 'name' => 'Strontium', 'concatenated_name' => 'Blue - Strontium'),
-    800 => array( 'id' => 800, 'parent_id' => 750, 'name' => 'Nickel', 'concatenated_name' => 'Blue - Nickel'),
-    810 => array( 'id' => 810, 'parent_id' => 750, 'name' => 'Stain', 'concatenated_name' => 'Blue - Stain'),
-    820 => array( 'id' => 820, 'parent_id' => 460, 'name' => 'Purple', 'concatenated_name' => 'Purple'),
-    830 => array( 'id' => 830, 'parent_id' => 820, 'name' => 'Magnesium Matte', 'concatenated_name' => 'Purple - Magnesium Matte'),
-    840 => array( 'id' => 840, 'parent_id' => 820, 'name' => 'Nickel', 'concatenated_name' => 'Purple - Nickel'),
-    850 => array( 'id' => 850, 'parent_id' => 820, 'name' => 'Manganese', 'concatenated_name' => 'Purple - Manganese'),
-    860 => array( 'id' => 860, 'parent_id' => 460, 'name' => 'Matte', 'concatenated_name' => 'Matte'),
-    870 => array( 'id' => 870, 'parent_id' => 860, 'name' => 'Magnesium Matte', 'concatenated_name' => 'Matte - Magnesium Matte'),
-    880 => array( 'id' => 880, 'parent_id' => 460, 'name' => 'Black', 'concatenated_name' => 'Black'),
-    890 => array( 'id' => 890, 'parent_id' => 880, 'name' => 'Slip-Based', 'concatenated_name' => 'Black - Slip-Based'),
-    900 => array( 'id' => 900, 'parent_id' => 880, 'name' => 'Glossy', 'concatenated_name' => 'Black - Glossy'),
-    910 => array( 'id' => 910, 'parent_id' => 880, 'name' => 'Satin', 'concatenated_name' => 'Black - Satin'),
-    920 => array( 'id' => 920, 'parent_id' => 460, 'name' => 'Yellow', 'concatenated_name' => 'Yellow'),
-    930 => array( 'id' => 930, 'parent_id' => 920, 'name' => 'Iron', 'concatenated_name' => 'Yellow - Iron'),
-    940 => array( 'id' => 940, 'parent_id' => 920, 'name' => 'Barium', 'concatenated_name' => 'Yellow - Barium'),
-    950 => array( 'id' => 950, 'parent_id' => 920, 'name' => 'Manganese', 'concatenated_name' => 'Yellow - Manganese'),
-    960 => array( 'id' => 960, 'parent_id' => 920, 'name' => 'Stain', 'concatenated_name' => 'Yellow - Stain'),
-    970 => array( 'id' => 970, 'parent_id' => 920, 'name' => 'Nickel', 'concatenated_name' => 'Yellow - Nickel'),
-    980 => array( 'id' => 980, 'parent_id' => 460, 'name' => 'Crystalline', 'concatenated_name' => 'Crystalline'),
-    990 => array( 'id' => 990, 'parent_id' => 980, 'name' => 'Micro', 'concatenated_name' => 'Crystalline - Micro'),
-    1000 => array( 'id' => 1000, 'parent_id' => 980, 'name' => 'Aventurine', 'concatenated_name' => 'Crystalline - Aventurine'),
-    1010 => array( 'id' => 1010, 'parent_id' => 980, 'name' => 'Manganese', 'concatenated_name' => 'Crystalline - Manganese'),
-    1020 => array( 'id' => 1020, 'parent_id' => 980, 'name' => 'Macro', 'concatenated_name' => 'Crystalline - Macro'),
-    1030 => array( 'id' => 1030, 'parent_id' => 460, 'name' => 'Single-Fire', 'concatenated_name' => 'Single-Fire'),
-    1040 => array( 'id' => 1040, 'parent_id' => 460, 'name' => 'Wood', 'concatenated_name' => 'Wood'),
-    1050 => array( 'id' => 1050, 'parent_id' => 460, 'name' => 'Salt & Soda', 'concatenated_name' => 'Salt & Soda'),
-    1055 => array( 'id' => 1055, 'parent_id' => 460, 'name' => 'Raku', 'concatenated_name' => 'Raku'),
-    1060 => array( 'id' => 1060, 'parent_id' => 460, 'name' => 'Ash', 'concatenated_name' => 'Ash'),
-    1070 => array( 'id' => 1070, 'parent_id' => 1060, 'name' => 'Nuka', 'concatenated_name' => 'Ash - Nuka'),
-    1080 => array( 'id' => 1080, 'parent_id' => 1060, 'name' => 'Synthetic/Fake', 'concatenated_name' => 'Ash - Synthetic/Fake'),
-    1090 => array( 'id' => 1090, 'parent_id' => 1060, 'name' => 'Slip-Based', 'concatenated_name' => 'Ash - Slip-Based'),
-    1100 => array( 'id' => 1100, 'parent_id' => 460, 'name' => 'Majolica', 'concatenated_name' => 'Majolica'),
-    1110 => array( 'id' => 1110, 'parent_id' => 460, 'name' => 'Bristol', 'concatenated_name' => 'Bristol'),
-    1130 => array( 'id' => 1130, 'parent_id' => 460, 'name' => 'Specialty', 'concatenated_name' => 'Specialty'),
-    1140 => array( 'id' => 1140, 'parent_id' => 1130, 'name' => 'Crackle', 'concatenated_name' => 'Specialty - Crackle'),
-    1150 => array( 'id' => 1150, 'parent_id' => 1130, 'name' => 'Crawling', 'concatenated_name' => 'Specialty - Crawling'),
-    1155 => array( 'id' => 1155, 'parent_id' => 1130, 'name' => 'Gloop', 'concatenated_name' => 'Specialty - Gloop'),
-    1160 => array( 'id' => 1160, 'parent_id' => 1130, 'name' => 'Crater', 'concatenated_name' => 'Specialty - Crater'),
-    1170 => array( 'id' => 1170, 'parent_id' => 1130, 'name' => 'Metallic', 'concatenated_name' => 'Specialty - Metallic'),
-    1180 => array( 'id' => 1180, 'parent_id' => 100, 'name' => 'Refractory', 'concatenated_name' => 'Refractory'),
+    '470' => ['470'],
+    '480' => ['480'],
+    '490' => ['490', '500', '510', '520', '530', '533', '535', '540', '550', '560', '570', '580', '585'],
+    '500' => ['500', '510', '520', '530', '533'],
+    '510' => ['510'],
+    '520' => ['520'],
+    '530' => ['530'],
+    '533' => ['533'],
+    '535' => ['535'],
+    '540' => ['540'],
+    '550' => ['550'],
+    '560' => ['560'],
+    '570' => ['570'],
+    '580' => ['580'],
+    '585' => ['585'],
+    '590' => ['590', '600', '610', '620', '630'],
+    '600' => ['600'],
+    '610' => ['610'],
+    '620' => ['620'],
+    '630' => ['630'],
+    '635' => ['635', '640', '650', '660', '670', '673', '675'],
+    '640' => ['640', '650', '660', '670'],
+    '650' => ['650'],
+    '660' => ['660'],
+    '670' => ['670'],
+    '673' => ['673'],
+    '675' => ['675'],
+    '680' => ['680', '690', '700', '710', '720', '730', '740'],
+    '690' => ['690'],
+    '700' => ['700'],
+    '710' => ['710'],
+    '720' => ['720'],
+    '730' => ['730'],
+    '740' => ['740'],
+    '745' => ['745'],
+    '750' => ['750', '760', '770', '780', '790', '800', '810'],
+    '760' => ['760'],
+    '770' => ['770'],
+    '780' => ['780'],
+    '790' => ['790'],
+    '800' => ['800'],
+    '810' => ['810'],
+    '820' => ['820', '830', '840', '850'],
+    '830' => ['830'],
+    '840' => ['840'],
+    '850' => ['850'],
+    '860' => ['860', '870'],
+    '870' => ['870'],
+    '880' => ['880', '890', '900', '910'],
+    '890' => ['890'],
+    '900' => ['900'],
+    '910' => ['910'],
+    '920' => ['920', '930', '940', '950', '960', '970'],
+    '930' => ['930'],
+    '940' => ['940'],
+    '950' => ['950'],
+    '960' => ['960'],
+    '970' => ['970'],
+    '980' => ['980', '990', '1000', '1010', '1020'],
+    '990' => ['990'],
+    '1000' => ['1000'],
+    '1010' => ['1010'],
+    '1020' => ['1020'],
+    '1030' => ['1030'],
+    '1040' => ['1040'],
+    '1050' => ['1050'],
+    '1055' => ['1055'],
+    '1060' => ['1060', '1070', '1080', '1090'],
+    '1070' => ['1070'],
+    '1080' => ['1080'],
+    '1090' => ['1090'],
+    '1100' => ['1100'],
+    '1110' => ['1110'],
+    '1120' => ['1120'],
+    '1130' => ['1130', '1140', '1150', '1155', '1160', '1170'],
+    '1140' => ['1140'],
+    '1150' => ['1150'],
+    '1155' => ['1155'],
+    '1160' => ['1160'],
+    '1170' => ['1170'],
+    '1180' => ['1180']
 ];
 
-$type_lookup = [
+
+$cone_lookup = [
     1 => '022',
     2 => '021',
     3 => '020',
@@ -150,19 +152,32 @@ while (($data = fgetcsv($umf_handle, 1000, ",")) !== FALSE) {
     // 'cone', 'SiO2', 'Al2O3', 'B2O3', 'R2O', 'RO'
     $type = strval($data[0]);
     $cone = strval($data[1]);
-    $sio2 = strval($data[2]);
-    $al2o3 = strval($data[3]);
+    $sio2 = number_format(round($data[2]*2)/2, 1);
+    $al2o3 = number_format((float)$data[3], 1);
     // $sio2 = number_format(round($data[1]*2)/2, 1); // 0.0, 0.5, 1.0, etc.
     // $al2o3 = number_format($data[2], 1); // 0.1, 0.2, etc.
-    $b2o3 = $data[4];
+    // $b2o3 = $data[4];
 
-    if ($sio2 <= 8 && $al2o3 <= 1) {
-        if (!isset($heatmaps[$type])) { $heatmaps[$type] = []; }
-        if (!isset($heatmaps[$type][$sio2])) { $heatmaps[$type][$sio2] = []; }
-        if (!isset($heatmaps[$type][$sio2][$al2o3])) { $heatmaps[$type][$sio2][$al2o3] = 0; }
-        if (!isset($all_types[$sio2][$al2o3])) { $all_types[$sio2][$al2o3] = 0; }
-        $heatmaps[$type][$sio2][$al2o3] = $heatmaps[$type][$sio2][$al2o3] + 1;
-        $all_types[$sio2][$al2o3] = $all_types[$sio2][$al2o3] + 1;
+    if ($type == '460') {
+        // generic "Glaze" type
+        if ($sio2 <= 8 && $al2o3 <= 1) {
+            $all_types[$sio2][$al2o3] = $all_types[$sio2][$al2o3] + 1;
+        }
+    } else {
+        $subtypes = $glaze_types[$type];
+
+        if ($sio2 <= 8 && $al2o3 <= 1) {
+            foreach($subtypes as $subtype) {
+                if (!isset($heatmaps[$subtype])) { $heatmaps[$subtype] = []; }
+                if (!isset($heatmaps[$subtype][$sio2])) { $heatmaps[$subtype][$sio2] = []; }
+                if (!isset($heatmaps[$subtype][$sio2][$al2o3])) { $heatmaps[$subtype][$sio2][$al2o3] = 0; }
+                $heatmaps[$subtype][$sio2][$al2o3] = $heatmaps[$subtype][$sio2][$al2o3] + 1;
+            }
+        }
+        if ($sio2 <= 8 && $al2o3 <= 1) {
+            if (!isset($all_types[$sio2][$al2o3])) { $all_types[$sio2][$al2o3] = 0; }
+            $all_types[$sio2][$al2o3] = $all_types[$sio2][$al2o3] + 1;
+        }
     }
 }
 fclose($umf_handle);
@@ -177,11 +192,9 @@ foreach ($glaze_types as $key => $type) {
     fclose($fp);
 }
 
-$fp = fopen('csv/all.csv', 'w');
+$fp = fopen('csv_types/all.csv', 'w');
 for ($sio2 = 0; $sio2 <= 8; $sio2 += 0.1) {
     $sio2val = number_format(round($sio2*2)/2, 1);
     fputcsv($fp, array_merge([$sio2val], $all_types[$sio2val]));
 }
 fclose($fp);
-
-echo 'here';
